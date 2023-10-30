1a90c757040a1517b1f77c3e127250d4e6150733
BardaK03 <bardadarius6@gmail.com>
2023-10-30 19:02:23 +0200

    lab

---
+++ src/ro/uoradea/Dog.java
@@ -0,0 +1,13 @@
+package ro.uoradea;
+
+public class Dog {
+    private String name;
+    private String breed;
+    public Dog(String n,String b){
+        name = n;
+        breed = b;
+        public String getname(){return name;
+    } public void setName(String name)
+        {this.name=name;
+        }
+}
---
+++ src/ro/uoradea/labu3.java
@@ -0,0 +1,8 @@
+package ro.uoradea;
+public class labu3 {
+    public static void main (String[] args){
+
+
+    }
+
+}
